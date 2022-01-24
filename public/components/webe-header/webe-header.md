6cf702d4de8d7c2fac0482284a9ca9b00f1bceb6
gordonrmullenjr@gmail.com <hoot.webelistics@gmail.com>
2022-01-23 20:10:41 -0500

    2022_01_23:20:10:40
    Signed-off-by: gordonrmullenjr@gmail.com <hoot.webelistics@gmail.com>

---
+++ public/components/webe-header/webe-header.md
@@ -0,0 +1,23 @@
+<!--@ ::::::::: Header  Web  Component :::::::::: -->
+<!--~ :::::::::: 2022_01_23 ^ 12:29:00 :::::::::: -->
+
+<!--  -->
+<!--  -->
+
+### Backlog Checklist
+
+1.  -   [] Create a new component called "webe-header.js"
+2.  -   [ ] Create a new component called "webe-header.html"
+3.  -   [ ] Create a new component called "webe-header.css"
+4.  Create a new component called "webe-header.md"
+5.  Create title favicon of HOOT Head
+6.  Create HOOT Logo .png sized with largest Aspect.ratio sized
+7.  Research best CSS approach: Grid or Flexbox ?
+8.  Implement ShadowDOM for CSS encapsulation within webe-header.js
+9.  Research best CSS Integration: CSS Modules or CSS Inline ?
+10. Research Google Fonts for preloading and best-case usage.
+11.
+
+&check; - html checkbox example
+:white_check_mark: - emoji checkbox example
+&#9746; - unicode checkbox example
--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,6 +1,36 @@
-function print(value) {
-	console.log(value);
-}
+//@ :::: HOOT Webelistics®  Web Application Development  :::: @//
+//@ :::: Gordon R. Mullen Jr  'Date: 2022/01/20'   :::: @//
 
-console.dir(print);
-print('Hello World');
+//~ :::: Main Application Index.js File :::: ~//
+
+'use strict';
+
+const init = () => {
+	const head = document.querySelector('head');
+	const body = document.querySelector('body');
+
+	// Set Multiple Attributes
+	const setAttributes = (el, attrs) => {
+		for (let key in attrs) {
+			el.setAttribute(key, attrs[key]);
+		}
+	};
+	// generate script tag to bring in the webe-header component module into the DOM
+	const script = document.createElement('script');
+	setAttributes(script, {
+		src: '/public/components/webe-header/webe-header.js',
+		type: 'module',
+		content: 'text/javascript',
+	}); // going to import the CSS module from inside component's JS module
+	head.appendChild(script);
+
+	const webeHeader = document.createElement('webe-header');
+	setAttributes(webeHeader, {
+		id: 'webe-header',
+		class: 'webe-header',
+		alt: 'Header Web Component',
+	});
+	body.appendChild(webeHeader);
+};
+
+document.addEventListener('DOMContentLoaded', init);
